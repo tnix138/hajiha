580673d981ad362d489766bf2837e6ed18c2d7f8
amirali <amiralikaramijarahi@gmail.com>
2023-01-22 09:22:04 -0800

    test 2

---
+++ mashin hesab amirali/mashin hesab amirali/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mashin_hesab_amirali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(129201);
+        }
+    }
+}
